7d9c1210b2430786a9d8ed07ffd78cc44e76c31a
Tanishq7361 <tanishq7361@gmail.com>
2026-06-14 23:10:14 +0530

    added authentication request dtos

---
+++ backend/src/main/java/com/equisplit/dto/request/LoginRequest.java
@@ -0,0 +1,28 @@
+package com.equisplit.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class LoginRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Size(max = 255)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 255, message = "Password must be at least 8 characters")
+    private String password;
+}
---
+++ backend/src/main/java/com/equisplit/dto/request/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.equisplit.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 120, message = "Name cannot exceed 120 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Size(max = 255)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 255, message = "Password must be at least 8 characters")
+    private String password;
+}
